78d9ba577dfd6f07aa0e81e37e93c5c0bc14a6c7
Akash shah <shah.akash@flotomate.com>
2019-03-14 17:45:37 +0530

    update state

--- react-my-first-app/src/App.js
+++ react-my-first-app/src/App.js
@@ -2,14 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 class App extends Component {
+  state={
+    Person:[
+      {name:'Akash Shah',age:25},
+      {name:'abc',age:24},
+      {name:'xyz',age:26}
+    ]
+  }
+   styles = {
+    width: '150px',
+    height: '50px',
+    
+  };
+  switchNameHandler =()=>{
+    console.log('was Clicked');
+   //Don't do this this will not change value
+   // this.state.Person[0].name='pqr'; 
+   this.setState({Person:[   
+     {name:'pqr',age:25},
+     {name:'abc',age:24},
+     {name:'xyz',age:26}]})
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
          <h1>Hi, I am new to React App.</h1>
-         <Person name="Akash" age="25"/>
-         <Person name="abc" age="24">My hobbies: Gamming, Coding</Person>
-         <Person name="xyz" age="25"/>
+         <button style={this.styles} onClick={this.switchNameHandler}>Swich Name</button>
+         <Person name={this.state.Person[0].name} age={this.state.Person[0].age}/>
+         <Person name={this.state.Person[1].name} age={this.state.Person[1].age}>My hobbies: Gamming, Coding</Person>
+         <Person  name={this.state.Person[2].name} age={this.state.Person[2].age}/>
         </header>
       </div>
       );
